00d463be6c32e97ca7a8ba7bb5098191a118d794
Will Grathwohl <wgrathwohl@gmail.com>
2019-12-15 15:37:55 -0500

    i think i got this shit together now

--- flow_utils.py
+++ flow_utils.py
@@ -1,7 +1,13 @@
+"""
+Written by big willy copyright big willy incorporated
+"""
+
 import jax.numpy as np
 from jax import random
 from jax.experimental import stax
-from jax.experimental.stax import Dense, Relu
+from jax.experimental.stax import Dense, Relu, Conv
+from jax.nn import log_sigmoid
+from utils import squeeze2d, unsqueeze2d
 
 
 """
@@ -34,6 +40,8 @@ reverse_fn(params, next_sample, next_logp):
 """
 Utils for chaining together flows
 """
+
+
 def init_flow_chain(rng, init_fns, init_params, init_batch=None):
     assert len(init_fns) == len(init_params)
     p_chain, f_chain, r_chain = [], [], []
@@ -59,6 +67,77 @@ def init_flow_chain(rng, init_fns, init_params, init_batch=None):
         return x, logp
 
     return p_chain, chain_forward, chain_reverse
+
+
+def init_factor_out(flow_params, flow_forward, flow_reverse, split_fn, rejoin_fn):
+    """
+    takes an existing flow and turns it into a flow which factors out dimensions
+    """
+    def forward(params, x, prev_logp=0.):
+        y, delta_logp = flow_forward(params, x, prev_logp=prev_logp)
+        y_keep, y_factorout = split_fn(y)
+        return y_keep, delta_logp, y_factorout
+
+    def reverse(params, y_next, prev_logp=0., y_factorout=None):
+        y = rejoin_fn(y_next, y_factorout)
+        return flow_reverse(params, y, prev_logp=prev_logp)
+
+    return flow_params, forward, reverse
+
+
+def init_factor_out_chain(rng, init_fns, init_params, split_fn, rejoin_fn, init_batch=None):
+    assert len(init_fns) == len(init_params)
+    p_chain, f_chain, r_chain = [], [], []
+    for init_fn, init_param in zip(init_fns, init_params):
+        rng, srng = random.split(rng)
+        p, f, r = init_fn(srng, *init_param, init_batch=init_batch)
+        p, f, r = init_factor_out(p, f, r, split_fn, rejoin_fn)
+        p_chain.append(p)
+        f_chain.append(f)
+        r_chain.append(r)
+        if init_batch is not None:
+            init_batch, _, __ = f(p, init_batch, 0.)
+
+    def chain_forward(params, prev_sample, prev_logp=0.):
+        x_next, logp = prev_sample, prev_logp
+        zs = []
+        for p, f in zip(params, f_chain):
+            x_next, logp, x_factorout = f(p, x_next, logp)
+            zs.append(x_factorout)
+        zs.append(x_next)
+        return zs, logp
+
+    def chain_reverse(params, zs, next_logp=0.):
+        zs, z_next = zs[:-1], zs[-1]  # split off the final z
+        logp = next_logp
+        for p, r, z_factorout in reversed(list(zip(params, r_chain, zs))):
+            z_next, logp = r(p, z_next, logp, z_factorout)
+        return x, logp
+
+    return p_chain, chain_forward, chain_reverse
+
+
+"""
+Some basic split/rejoin fns
+"""
+
+
+def split_dims(x):
+    d = x.shape[1] // 2
+    return x[:, :d], x[:, d:]
+
+
+def rejoin_dims(x1, x2):
+    return np.concatenate([x1, x2], 1)
+
+
+def split_channels(x):
+    c = x.shape[3] // 2
+    return x[:, :, :, :c], x[:, :, :, c:]
+
+
+def rejoin_channels(x1, x2):
+    return np.concatenate([x1, x2], 3)
 
 
 """
@@ -144,8 +223,143 @@ def init_actnorm(rng, init_batch=None):
 
 
 """
+Convolutional Actnorm
+"""
+
+
+def init_conv_actnorm(rng, init_batch=None):
+    assert init_batch is not None, "Actnorm requires data-dependent init"
+    mu, sig = np.mean(init_batch, axis=(0, 1, 2)), np.std(init_batch, axis=(0, 1, 2))
+    log_scale = np.log(sig)
+    params = (mu, log_scale)
+
+    def actnorm_forward(params, prev_sample, prev_logp=0.):
+        mu, log_scale = params
+        y = (prev_sample - mu[None, None, None, :]) * np.exp(-log_scale)[None, None, None, :]
+        b, h, w, c = prev_sample.shape
+        return y, prev_logp - np.sum(log_scale) * w * h
+
+    def actnorm_reverse(params, next_sample, next_logp=0.):
+        mu, log_scale = params
+        x = next_sample * np.exp(log_scale)[None, None, None, :] + mu[None, None, None, :]
+        b, h, w, c = next_sample.shape
+        return x, next_logp + np.sum(log_scale) * w * h
+
+    return params, actnorm_forward, actnorm_reverse
+
+
+"""
+Squeeze Layers
+"""
+
+
+def init_squeeze(rng, init_batch=None):
+    params = ()
+
+    def forward(params, prev_sample, prev_logp=0.):
+        return squeeze2d(prev_sample), prev_logp
+
+    def reverse(params, next_sample, next_logp=0.):
+        return unsqueeze2d(next_sample), next_logp
+
+    return params, forward, reverse
+
+
+"""
+Convolutional Coupling Layers
+"""
+
+
+def init_conv_affine_coupling(rng, in_shape, n_channels, flip, sigmoid=True, init_batch=None):
+    """
+    in_shape: tuple of (h, w, c)
+    """
+    h, w, c = in_shape
+    half_c = c // 2
+    net_init, net_apply = stax.serial(Conv(n_channels, 3), Relu, Conv(n_channels, 3), Relu, Conv(c, 3))
+    _, net_params = net_init(rng, (-1, h, w, half_c))
+
+    def shift_and_log_scale_fn(net_params, x1):
+        s = net_apply(net_params, x1)
+        return np.split(s, 2, axis=3)
+
+    def forward(net_params, prev_sample, prev_logp=0.):
+        x1, x2 = prev_sample[:, :half_c], prev_sample[:, half_c:]
+        if flip:
+            x2, x1 = x1, x2
+        shift, log_scale = shift_and_log_scale_fn(net_params, x1)
+        if sigmoid:
+            log_scale = log_sigmoid(log_scale + 2.)
+        y2 = x2 * np.exp(log_scale) + shift
+        if flip:
+            x1, y2 = y2, x1
+        y = np.concatenate([x1, y2], axis=-1)
+        return y, prev_logp + np.sum(log_scale, axis=-1)
+
+    def reverse(net_params, next_sample, next_logp=0.):
+        y1, y2 = next_sample[:, :half_c], next_sample[:, half_c:]
+        if flip:
+            y1, y2 = y2, y1
+        shift, log_scale = shift_and_log_scale_fn(net_params, y1)
+        if sigmoid:
+            log_scale = log_sigmoid(log_scale + 2.)
+        x2 = (y2 - shift) * np.exp(-log_scale)
+        if flip:
+            y1, x2 = x2, y1
+        x = np.concatenate([y1, x2], axis=-1)
+        return x, next_logp - np.sum(log_scale, axis=-1)
+
+    return net_params, forward, reverse
+
+
+"""
+High level convolutional flows
+"""
+
+
+def init_conv_flow_step(rng, in_shape, n_channels, flip, init_batch=None):
+    """ One step of flow actnorm --> affine coupling"""
+    return init_flow_chain(rng,
+                           [init_conv_actnorm, init_conv_affine_coupling],
+                           [(), (in_shape, n_channels, flip)],
+                           init_batch=init_batch)
+
+
+def init_conv_flow_block(rng, in_shape, n_steps, n_channels, init_batch=None):
+    """ Flow block: squeeze --> n_steps * flow_step """
+    flip = False
+    init_fns = [init_squeeze]
+    init_params = [()]
+    h, w, c = in_shape
+    squeeze_shape = h // 2, w // 2, c * 4
+    for _ in range(n_steps):
+        init_fns.append(init_conv_flow_step)
+        init_params.append((squeeze_shape, n_channels, flip))
+        flip = not flip
+    return init_flow_chain(rng, init_fns, init_params, init_batch=init_batch)
+
+
+def init_multiscale_conv_flow(rng, in_shape, n_channels, n_blocks, n_steps, init_batch=None):
+    """ Creates a multi-scale convolutional normalizing flow like Glow but currently no 1x1 convolutions """
+    params = []
+    chain = []
+    cur_shape = in_shape
+    for _ in range(n_blocks):
+        chain.append(init_conv_flow_block)
+        params.append(
+            (cur_shape, n_steps, n_channels)
+        )
+        h, w, c = cur_shape
+        cur_shape = h // 2, w // 2, c * 4
+
+    return init_factor_out_chain(rng, chain, params, split_channels, rejoin_channels, init_batch=init_batch)
+
+
+"""
 Utilities to build functions for training and eval
 """
+
+
 def make_log_prob_fn(forward_fn, base_dist_log_prob):
     def log_prob(p, x):
         z, logp = forward_fn(p, x)
